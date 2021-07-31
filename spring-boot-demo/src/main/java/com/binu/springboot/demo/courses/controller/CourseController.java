abc5c866950c42fd704075656e1fb421a3db8ebd
bthank <bthankachan@yahoo.com>
2021-07-31 17:23:11 -0400

    Added get specific course endpoint

--- spring-boot-demo/src/main/java/com/binu/springboot/demo/courses/controller/CourseController.java
+++ spring-boot-demo/src/main/java/com/binu/springboot/demo/courses/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,20 @@ public class CourseController {
 
 	@GetMapping("/courses")
 	public List<Course> getallCourses() {
-		return Arrays.asList(new Course(1L,"Microservice Architectures","Mark Meadows"));
+		return Arrays.asList(new Course(1L,"Microservice Architectures","Mark Meadows"),
+				new Course(2L,"Advanced Java","Peter Travers"),
+				new Course(3L,"AWS Fundamentals","Kal Summers"));
+	}
+	
+	@GetMapping("/courses/{id}")
+	public Course getCourse(@PathVariable long id) {
+		if (id == 1) {
+			return new Course(1L,"Microservice Architectures","Mark Meadows");
+		} else if (id == 2){
+			return new Course(2L,"Advanced Java","Peter Travers");
+		} else {
+			return new Course(3L,"AWS Fundamentals","Kal Summers");
+		}
 	}
 	
 	
